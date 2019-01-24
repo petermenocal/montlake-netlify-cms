f7782cd95fc2c1a342b7322ae60c2db5b44b77f6
Peter Menocal <peter@geekypixel.com>
2019-01-24 10:28:37 -0800

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,85 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import PropTypes from "prop-types";
+import React from "react";
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+    console.log(data);
+    const image = data.allFile.edges[0].node;
     return (
       <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundSize: `cover`,
+            backgorundPosition: `center center`,
+            backgroundImage: `url(${
+              !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`
+          }}
+        >
+          <h2
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+              color: "white",
+              padding: "1rem",
+              textAlign: "center"
+            }}
+          >
+            Vision &middot; Innovation &middot; Science
+          </h2>
+        </div>
         <section className="section">
           <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
+            {posts.map(({ node: post }) => (
+              <div
+                className="content"
+                style={{ border: "1px solid #333", padding: "2em 4em" }}
+                key={post.id}
+              >
+                <p>
+                  <Link className="has-text-primary" to={post.fields.slug}>
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-small" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </div>
+            ))}
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -75,5 +96,18 @@ export const pageQuery = graphql`
         }
       }
     }
+    allFile(
+      filter: { id: { regex: "/fe8ae2e7-5c0f-562d-a85a-f7ff27ba59ca/g" } }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 2000, maxHeight: 1050) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
   }
-`
+`;
--- src/pages/products/index.md
+++ src/pages/products/index.md
@@ -1,8 +1,8 @@
 ---
-templateKey: 'product-page'
+templateKey: "product-page"
 path: /products
-title: Our Coffee
-image: /img/jumbotron.jpg
+title: Vision • Innovation • Science
+image: /img/header-main_hero.jpg
 heading: What we offer
 description: >-
   Kaldi is the ultimate spot for coffee lovers who want to learn about their
@@ -83,19 +83,19 @@ pricing:
         - Green or roasted beans"
         - One or two varieties of beans"
       plan: Small
-      price: '50'
-    - description: 'Great for avid drinkers, java-loving couples and bigger crowds'
+      price: "50"
+    - description: "Great for avid drinkers, java-loving couples and bigger crowds"
       items:
         - 6 lbs of coffee per month
         - Green or roasted beans
         - Up to 4 different varieties of beans
       plan: Big
-      price: '80'
+      price: "80"
     - description: Want a few tiny batches from different varieties? Try our custom plan
       items:
         - Whatever you need
         - Green or roasted beans
         - Unlimited varieties
       plan: Custom
-      price: '??'
+      price: "??"
 ---
